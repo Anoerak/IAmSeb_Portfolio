c4f35b6e90215c5e958daa554c2fef4edb32e4f2
Anørak <seb@iamseb.dev>
2023-08-29 11:59:27 +0200

    Started the work on Symfony

--- Symfony/src/Controller/HomeController.php
+++ Symfony/src/Controller/HomeController.php
@@ -9,9 +9,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('home/home.html.twig', [
+        return $this->render('pages/home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/projects', name: 'app_projects')]
+    public function projects(): Response
+    {
+        return $this->render('pages/projects/projects.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('pages/about/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact/contact.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
